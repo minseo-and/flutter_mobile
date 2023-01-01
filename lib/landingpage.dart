5f36b2c38d8972c4e05e50142eb054e43708e273
minseo-and <msp05070478@gmail.com>
2023-01-01 21:26:23 +0900

    add logo image

--- lib/landingpage.dart
+++ lib/landingpage.dart
@@ -30,8 +30,21 @@ class _LandingPageState extends State<LandingPage> {
           Container(
               width: MediaQuery.of(context).size.width,
               height: MediaQuery.of(context).size.height,
+              decoration: const BoxDecoration(
+                gradient: LinearGradient(
+                  colors: [Color(0xff1EB79A), Color(0xff00E6A4), Color(0xff3FE48A),],
+                  begin: Alignment.topCenter,
+                  end: Alignment.bottomCenter
+                )
+              ),
           ),
-          CircularProgressIndicator()
+          Container(
+            margin: EdgeInsets.all(100),
+            child: const Image(
+              image: AssetImage('assets/image/logo.png'),
+              fit: BoxFit.fitWidth,
+            ),
+          )
         ],
       )
     );

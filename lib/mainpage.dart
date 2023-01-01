6073777db4baf3cf22c9dd678e86be6d0b10f51b
minseo-and <msp05070478@gmail.com>
2023-01-01 20:53:14 +0900

    add main_page

---
+++ lib/mainpage.dart
@@ -0,0 +1,17 @@
+
+import 'package:flutter/material.dart';
+
+class MainPage extends StatefulWidget {
+
+  _MainPageState createState() => _MainPageState();
+
+}
+
+class _MainPageState extends State<MainPage> {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw Scaffold();
+  }
+
+}
